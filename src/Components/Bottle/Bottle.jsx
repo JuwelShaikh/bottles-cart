1ebc43cbc096356b6443b1d172fa4f33f1d13ad1
JuwelShaikh <juwelshaikh04@gmail.com>
2024-06-30 16:26:32 +0600

    Bottle css added

--- src/Components/Bottle/Bottle.jsx
+++ src/Components/Bottle/Bottle.jsx
@@ -3,11 +3,12 @@ import './Bottle.css';
 const Bottle = ({ bottle }) => {
     const { name, price, img, seller } = bottle;
     return (
-        <div>
+        <div className='bottle-container'>
             <h4>Name: {name}</h4>
             <h5>Brand: {seller}</h5>
             <img src={img} alt="bottle pic" />
             <p>Price: ${price}</p>
+            <button>But now</button>
         </div>
     );
 };
