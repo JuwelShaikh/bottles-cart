04c6c7003e8ac15e15c00acce34db76ed941943e
JuwelShaikh <juwelshaikh04@gmail.com>
2024-06-30 16:31:58 +0600

    Complete

--- src/Components/Bottles/Bottles.jsx
+++ src/Components/Bottles/Bottles.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Bottle from "../Bottle/Bottle";
+import './Bottles.css'
 
 const Bottles = () => {
 
@@ -10,13 +11,15 @@ const Bottles = () => {
             .then(res => res.json())
             .then(data => setBottles(data))
     }, [])
-    
+
     return (
         <div>
             <h3>Bottles available: {bottles.length}</h3>
-            {
-                bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}/>)
-            }
+            <div className="bottles-container">
+                {
+                    bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle} />)
+                }
+            </div>
         </div>
     );
 };
